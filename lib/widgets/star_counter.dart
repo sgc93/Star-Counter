f20908510def510e0d33ee91ea6ab638422855b4
sgc93 <smachewgedefaw@gmail.com>
2023-10-07 04:08:28 -0900

    create a StarCounter Widget to fetch num of star

---
+++ lib/widgets/star_counter.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class StarCounter extends StatefulWidget {
+  const StarCounter({super.key});
+
+  @override
+  State<StarCounter> createState() => _StarCounterState();
+}
+
+class _StarCounterState extends State<StarCounter> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
