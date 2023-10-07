e6b14db59c065d7006976b4a493bee564f005ea0
sgc93 <smachewgedefaw@gmail.com>
2023-10-07 04:30:00 -0900

    fetch repo with github library

--- lib/widgets/star_counter.dart
+++ lib/widgets/star_counter.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:github/github.dart';
 
 class StarCounter extends StatefulWidget {
-  const StarCounter({super.key});
+  // full repository name : pp_name/repo_name
+  final String repoName;
+
+  // get repo name from HomePage Widget
+  const StarCounter({
+    super.key,
+    required this.repoName,
+  });
 
   @override
   State<StarCounter> createState() => _StarCounterState();
 }
 
 class _StarCounterState extends State<StarCounter> {
+  // instance of GitHub API client
+  late GitHub gitHub;
+
+  // instance of Repository information
+  Repository? repository;
+
+  // error message : repo not found!
+  String? errorMessage;
+
+  // initialize GitHub API client
+  @override
+  void initState() {
+    super.initState();
+    gitHub = GitHub();
+  }
+
+  Future<void> _fetchRepository() async {
+    if (widget.repoName.isNotEmpty) {
+      var repo = await gitHub.repositories
+          .getRepository(RepositorySlug.full(widget.repoName));
+      setState(() {
+        repository = repo;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
